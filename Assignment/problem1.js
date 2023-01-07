2cd66cf46ed94641575e42bbec5ff6249516e1a1
AlkaSinha5 <sinhaalka802211@gmail.com>
2023-01-07 13:47:57 +0530

    day2

---
+++ Assignment/problem1.js
@@ -0,0 +1,13 @@
+// Problem 1 : Check whether a number is Prime or not
+let x=13;
+let count=0;
+for(let i=0;i<=N;i++){
+  if(x%i==0){
+    count++;
+  }
+}
+if(count==2){
+  console.log("prime");
+}else{
+  console.log("not a prime")
+}
---
+++ Assignment/problem2.js
@@ -0,0 +1,11 @@
+// Check whether a string is palindrome or not.
+let str="masai";
+let bag="";
+for(let i=str.length-1;i>=0;i--){
+bag=bag+str[j];
+}
+if(str==bag){
+  console.log("is pelindrome");
+}else{
+  console.log("not a pelindrome");
+}
